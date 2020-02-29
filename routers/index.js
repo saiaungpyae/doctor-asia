987554ce027ade7c1af3829438a07a357f94e6f6
Sai Aung Pyae <saiaungpyae20@gmail.com>
2020-03-01 00:32:27 +0630

    add: default message for /

--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,9 @@ import {
 } from '../controllers/version.controller';
 
 export default app => {
+  app.get('/', (req, res) => {
+    res.json({ message: 'https://github.com/saiaungpyae/doctor-asia' });
+  });
   app.post('/object', postVersion);
   app.get('/object/:key', getVersionDetail);
 };
